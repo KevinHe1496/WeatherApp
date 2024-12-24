5e5883bfdd9b68effbae25fe7790d5cb1bedd94a
Kevin Heredia <kevin@ravecodesolutions.com>
2024-12-24 11:13:24 -0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Kevin Heredia on 24/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
